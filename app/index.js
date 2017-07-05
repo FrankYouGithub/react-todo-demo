2a96d2e8b1e19ab9b950bf34fbc51b4fa43710bf
youhuakun <2464280815@qq.com>
2017-07-05 15:26:00 +0800

    引入jQuery

--- app/index.js
+++ app/index.js
@@ -1,12 +1,9 @@
 import _ from 'lodash';
+import $ from 'jquery';
 function component() {
-  var element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+  var element = $('<div></div>');
+  element.html(_.join(['hello','webpack'], ' '))  
+  return element.get(0);
 }
 
 document.body.appendChild(component());
