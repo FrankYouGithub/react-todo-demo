3128e47c420dbc6235bc2c3f2fef278f1a40340c
youhuakun <2464280815@qq.com>
2017-07-05 23:06:10 +0800

    添加self

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import $ from 'jquery';
+import test from './self';
 function component() {
   var element = $('<div></div>');
   element.html(_.join(['hello','webpack'], ' '))  
   return element.get(0);
 }
-
+var str = test();
 document.body.appendChild(component());
+console.log(str);
---
+++ app/self.js
@@ -0,0 +1,5 @@
+function test(){
+    return 'hello everyone'
+}
+
+module.exports = test
