2e01ceb004aa5f708a4b12d129ede55429944eff
youhuakun <2464280815@qq.com>
2017-07-11 11:53:20 +0800

    react lifecycle

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Welcome from './welcome';
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('root2')
+//   );
+// }
+// setInterval(tick, 1000);
 
 ReactDOM.render(
-  <h1 id="title">hello world!</h1>,
+  <Welcome name="youhuakun"/>,
   document.getElementById('root2')
-);
+)
