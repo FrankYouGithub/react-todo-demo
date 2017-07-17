6e21f0a7d947e5450ba96150524de9363cf9377c
youhuakun <2464280815@qq.com>
2017-07-17 15:28:24 +0800

    标记已/未完成，删除TODO，更新CSS

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ class App extends Component {
     }
   }
   render() {
-    let todos = this.state.todoList.map((item,index)=>{
+    let todos = this.state.todoList.filter((item)=> !item.deleted).map((item,index)=>{
       return (
         <li key={index}>
-          <TodoItem todo={item}/>
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)} onDelete={this.delete.bind(this)}/>
         </li>
       )
     })
@@ -27,15 +27,32 @@ class App extends Component {
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)}/>
+          <TodoInput content={this.state.newTodo} onChange={this.changeTitle.bind(this)} onSubmit={this.addTodo.bind(this)}/>
         </div>
-        <ol>
+        <ol className="todoList">
           {todos}
         </ol>
       </div> 
     );
   }
+  delete(e,todo){
+    todo.deleted = true
+    this.setState(this.state)
+  }
+  toggle(e,todo){
+    todo.status = todo.status === 'completed'? '': 'completed'
+    this.setState(this.state)
+  }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
   addTodo(event){
+    if (event.target.value === '') {
+      return
+    }
      this.state.todoList.push({
        id: idMaker(),
        title: event.target.value,
