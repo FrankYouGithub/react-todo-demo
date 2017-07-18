3e9d33c83354029c7e8d7e2b073449d80a3b53ca
youhuakun <2464280815@qq.com>
2017-07-18 17:31:28 +0800

    新建 leanCloud.js 存放初始化代码

--- src/App.js
+++ src/App.js
@@ -6,16 +6,7 @@ import TodoItem from './TodoItem';
 import 'normalize.css';
 import './reset.css';
 import * as localStore from './localStore';
-import AV from 'leancloud-storage';
 
-
-var APP_ID = 'I0qC3ES4VhYcgUHfbt8KJFBh-gzGzoHsz';
-var APP_KEY = 'eBSC84SKjeDTmeeFakEeaR3g';
-
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
 
 class App extends Component {
   constructor(props){
---
+++ src/leanCloud.js
@@ -0,0 +1,11 @@
+import AV from 'leancloud-storage';
+
+var APP_ID = 'I0qC3ES4VhYcgUHfbt8KJFBh-gzGzoHsz';
+var APP_KEY = 'eBSC84SKjeDTmeeFakEeaR3g';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV
