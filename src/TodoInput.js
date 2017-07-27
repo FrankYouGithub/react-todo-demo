c469e7ce8e0b4d4bc3a64192b45e2ea59c6ab9fd
youhuakun <2464280815@qq.com>
2017-07-27 14:21:47 +0800

    就算一个组件有方法，也可以改写成一个函数

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
 import './TodoInput.css'
-export default class TodoInput extends Component {
-    render(){
-        return <input className="TodoInput" type="text" value={this.props.content} onChange={this.changeTitle.bind(this)} onKeyPress={this.submit.bind(this)}/>
-    }
-    submit(e){
-        if (e.key === "Enter") {
-            this.props.onSubmit(e)
-        }
-    }
-    changeTitle(e){
-        this.props.onChange(e)
-    }
+// export default class TodoInput extends Component {
+//     render(){
+//         return <input className="TodoInput" type="text" value={this.props.content} 
+//         onChange={this.changeTitle.bind(this)} onKeyPress={this.submit.bind(this)}/>
+//     }
+//     submit(e){
+//         if (e.key === "Enter") {
+//             this.props.onSubmit(e)
+//         }
+//     }
+//     changeTitle(e){
+//         this.props.onChange(e)
+//     }
     
+// }
+
+export default function(props){
+    return <input className="TodoInput" type="text" value={props.content} 
+        onChange={changeTitle.bind(this,props)} onKeyPress={submit.bind(this,props)}/>
+}
+function changeTitle(props,e){
+    props.onChange(e)
+}
+function submit(props,e){
+    if (e.key === 'Enter') {
+        props.onSubmit(e)
+    }
 }
